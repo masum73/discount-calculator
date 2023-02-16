b490135561ab04b8e3cd8afd44b48f34a146e7bd
Masum Rahman <masumrahman73@gmail.com>
2023-02-16 15:26:56 +0600

    almost easy discount calculator done

--- js/script.js
+++ js/script.js
@@ -13,9 +13,30 @@ document.getElementById('btn-clear').addEventListener('click',function(){
 
 //almost easy discount calculator
 
+document.getElementById('coupon-field').addEventListener('keyup', function(event){
+    const text = event.target.value;
+    const applyButton = document.getElementById('btn-apply-coupon');
+    
+    if(text==='DISC30'){
+        applyButton.removeAttribute('disabled');
+        
+        applyButton.addEventListener('click',function(){
+            let discount =  (1000*30)/100; 
+            const textareaField = document.getElementById('textareaFieldTwo');
+            textareaField.value = discount;
+        })
+    }else{
+        applyButton.setAttribute('disabled',true);
+    }
+})
 
-
-/* document.getElementById('btn-clear').addEventListener('click',function(){
+document.getElementById('btn-clear-two').addEventListener('click',function(){
+    const couponField = document.getElementById('coupon-field');
+    couponField.value = '';
+    
     const textareaField = document.getElementById('textareaFieldTwo');
     textareaField.value = '';
-}) */
+    
+    const applyButton = document.getElementById('btn-apply-coupon');
+    applyButton.setAttribute('disabled',true);
+})
